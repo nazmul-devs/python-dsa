8e94f52074e0692ed83ab35a672a76d74cd8dfa8
nazmul-devs <nazmulhosenm668@gmail.com>
2024-08-17 22:36:27 +0600

    commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello buddy!")
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if i!=j and nums[i]+nums[j]==target:
+                return [i, j]
+                
+
+    
+
+print(twoSum([3,3],6))
